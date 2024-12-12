bb907c3c69c8250a96851fd6cb07a479f31d02c6
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 23:53:24 +0530

    Add StatsCard component

---
+++ src/components/sections/about/StatsCard.tsx
@@ -0,0 +1,17 @@
+interface StatsCardProps {
+  value: string;
+  label: string;
+}
+
+export default function StatsCard({ value, label }: StatsCardProps) {
+  return (
+    <div className="bg-white rounded-md p-4 flex flex-col items-center justify-center gap-1 transition-all duration-300 hover:shadow-lg">
+      <span className="text-dark font-montserrat font-semibold text-3xl leading-tight">
+        {value}
+      </span>
+      <span className="text-gray-600/60 font-opensans text-xs">
+        {label}
+      </span>
+    </div>
+  );
+} 
