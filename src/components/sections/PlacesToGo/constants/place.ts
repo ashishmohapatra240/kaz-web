38e9df55b8e86a783fd1130af83d8248f8fbe83d
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 23:46:33 +0530

    Add PLACES constant for PlacesToGo section with details and images

---
+++ src/components/sections/PlacesToGo/constants/place.ts
@@ -0,0 +1,43 @@
+export const PLACES = [
+    {
+        id: 1,
+        title: "Deomali",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Deomali.jpg",
+        rating: 5
+    },
+    {
+        id: 2,
+        title: "Duduma",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Duduma.jpg",
+        rating: 2
+    },
+    {
+        id: 3,
+        title: "Balda",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Balda.jpg",
+        rating: 4
+    },
+    {
+        id: 1,
+        title: "Deomali",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Deomali.jpg",
+        rating: 5
+    },
+    {
+        id: 2,
+        title: "Duduma",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Duduma.jpg",
+        rating: 2
+    },
+    {
+        id: 3,
+        title: "Balda",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        image: "/images/PlacesToGo/Balda.jpg",
+        rating: 4
+    },]; 
