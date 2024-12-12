94c7f93a84fe048f8b44a1ba53ffcf4a85aa1633
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 23:54:00 +0530

    Add stats constant

---
+++ src/constants/about-stats.ts
@@ -0,0 +1,6 @@
+export const stats = [
+    { value: "105+", label: "Campsites" },
+    { value: "30+", label: "Amenities" },
+    { value: "1,3M", label: "Reservations" },
+    { value: "70+", label: "Year of camping" },
+  ];
