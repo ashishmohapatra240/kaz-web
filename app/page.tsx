d7d2dda324eec606a21cf530ca9938bdae771e6f
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 23:54:14 +0530

    Enhance Home page by adding new sections: PlacesToGo, CampInfrastructure, and AboutSection components for improved content presentation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { Hero } from "@/src/components/sections/Hero/Hero";
+import { PlacesToGo } from "@/src/components/sections/PlacesToGo/PlacesToGo";
+import CampInfrastructure from "../src/components/sections/camping/CampInfrastructure";
+import AboutSection from "@/src/components/sections/about/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <PlacesToGo />
+      <CampInfrastructure />
+      <AboutSection />
     </main>
   );
 }
