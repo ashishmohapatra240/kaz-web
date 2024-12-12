71cbffc4c9daf59b92218ddc1e06c044b49aa1bc
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-12 22:11:18 +0530

    Add navigation links constant for site navigation

---
+++ src/constants/navigation.ts
@@ -0,0 +1,6 @@
+export const navigationLinks = [
+    { label: 'Home', href: '/' },
+    { label: 'Gallery', href: '/gallery' },
+    { label: 'About', href: '/about' },
+    { label: 'Contact', href: '/contact' },
+];
