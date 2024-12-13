0fa9cebed30f526031993d260a16cb881dcc1507
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-13 14:28:05 +0530

    Integrate PhotoGallery component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/src/components/sections/Hero/Hero";
 import { Packages } from "@/src/components/sections/PlacesToGo/Packages";
 import CampInfrastructure from "../src/components/sections/camping/CampInfrastructure";
 import AboutSection from "@/src/components/sections/about/AboutSection";
+import PhotoGallery from "@/src/components/sections/Gallery/PhotoGallery";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Packages />
       <AboutSection />
+      <PhotoGallery />
       <CampInfrastructure />
     </main>
   );
