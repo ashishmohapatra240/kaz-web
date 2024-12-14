df4150c9425cee3e333417ea0f63145ed40ebb3c
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-14 21:24:12 +0530

    Add Testimonials section into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Packages } from "@/src/components/sections/PlacesToGo/Packages";
 import CampInfrastructure from "../src/components/sections/camping/CampInfrastructure";
 import AboutSection from "@/src/components/sections/about/AboutSection";
 import PhotoGallery from "@/src/components/sections/Gallery/PhotoGallery";
+import { Testimonials } from "@/src/components/sections/Testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutSection />
       <PhotoGallery />
       <CampInfrastructure />
+      <Testimonials />
     </main>
   );
 }
