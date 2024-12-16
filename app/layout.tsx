4d1cd6b64032f173dcb8ba0925a9302535a19bc9
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-17 01:39:17 +0530

    Refactor RootLayout to include session management and enhance structure
    - Changed RootLayout to an async function to fetch the user session using getServerSession.
    - Wrapped the main content with Providers to manage session context.
    - Added Toaster component for notifications and adjusted layout structure for better content organization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,32 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/src/components/layout/Navbar";
 import { Footer } from "@/src/components/Footer/Footer";
+import Providers from "./components/Providers";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Koraput Website",
   description: "Koraput Website",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        <Providers session={session}>
+          <Navbar />
+          <main className="pt-20 pb-20">{children}</main>
+          <Footer />
+          <Toaster position="top-center" />
+        </Providers>
       </body>
     </html>
   );
